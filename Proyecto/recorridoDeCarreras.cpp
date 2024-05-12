eb119c3b1650d77067b0aea50078afed8523a386
Adrián Ignacio Coello Santana <160319432+AdrianCoello@users.noreply.github.com>
2024-05-12 17:55:34 -0400

    Update recorridoDeCarreras.cpp

--- Proyecto/recorridoDeCarreras.cpp
+++ Proyecto/recorridoDeCarreras.cpp
@@ -1,8 +1,7 @@
-//UPB 
 #include<bits/stdc++.h>
 using namespace std;
 
-#define INF 100000010
+#define INF 30
 
 vector<pair<int, int>> grafo[18] = {{{1, 9}, {2, 14}, {3, 10}, {4, 15}},
                                      {{5, 13}, {6, 11}, {7, 8}, {13, 10}},
@@ -29,7 +28,7 @@ int visitados[18]; // marcamos si lo visitamos
 int distancia[18]; // guardamos su distancias para ir comparando
 int previo[18]; 
 
-void dijkstra(int verticeInicial)
+void dijkstra(int verticeInicial, int destination)
 {
     for (int i = 0; i < 18; i++)
     { // todos empiezan en infinito para comparar
@@ -37,21 +36,20 @@ void dijkstra(int verticeInicial)
         previo[i] = -1;
     }
     memset(visitados, 0, sizeof(visitados)); // Todos los nodos como no visitado
-    multiset<pair<int, int>> colaPrioridad;  // ordena por prioridad según las distancias
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> colaPrioridad;  // ordena por prioridad según las distancias
     distancia[verticeInicial] = 0;
                // no recorre nada todavía
-    colaPrioridad.insert(make_pair(0, verticeInicial));
+    colaPrioridad.push(make_pair(0, verticeInicial));
 
     while (!colaPrioridad.empty())
     {
-        pair<int, int> verticeActual = *colaPrioridad.begin(); // para dar el valor del primer datp de colaprioridad
-        colaPrioridad.erase(colaPrioridad.begin());            // borra al que acabamos de recibir su valor
+        pair<int, int> verticeActual = colaPrioridad.top(); // para dar el valor del primer datp de colaprioridad
+        colaPrioridad.pop();            // borra al que acabamos de recibir su valor
         int vertice = verticeActual.second;                    // el nodo de donde esta viniendo
         int peso = verticeActual.first;                        // distancia
         if (!visitados[vertice])
         {                              // no pasamos por este antes
             visitados[vertice] = true; // ya lo visitamos
-            cout << carreras[previo[destination]] << " -> ";
             for (int i = 0; i < grafo[vertice].size(); i++)
             {
                 int verticeVecino = grafo[vertice][i].second;
@@ -62,19 +60,18 @@ void dijkstra(int verticeInicial)
                     //
                     previo[verticeVecino]= vertice;
                     //
-                    colaPrioridad.insert(make_pair(distancia[verticeVecino], verticeVecino));
+                    colaPrioridad.push(make_pair(distancia[verticeVecino], verticeVecino));
                 }
             }
         }
     }
 }
 
-void print( int destination ){
-    if( previo[ destination ] != -1 )    //si aun poseo un vertice previo
-        print(previo[ destination ]);  //recursivamente sigo explorando
-    if (previo[ destination ] != -1) {
-        cout << carreras[previo[destination]] << " -> "; // mostrar el paso intermedio solo si existe
+void print(int destination) {
+    if (previo[destination] != -1) { // si aun poseo un vertice previo
+        print(previo[destination]);  // recursivamente sigo explorando
     }
+    cout << carreras[destination] << " -> "; // mostrar cada paso
 }
 
 int main()
@@ -84,17 +81,14 @@ int main()
     cout << "6: Ingeniería Comercial\n7: Psicología Organizacional\n8: Marketing y Logística\n9: Derecho\n10: Arquitectura\n";
     cout << "11: Economía\n12: Ingeniería Financiera\n13: Relaciones y Negocios Internacionales\n14: Ingeniería Electromecánica\n";
     cout << "15: Ingeniería de Sistemas Computacionales\n16: Ingeniería Industrial y de Sistemas\n17: Bioingeniería en Recursos Naturales\n";
-    cout << "Según la lista anterior, añade la carrera de tu interés como punto de partida: ";
-    int source, destination;
-    cin >> source;
-    source=source;
     cout << "Ahora añade la carrera final o la carrera que te gustaría conocer: ";
+    int destination;
     cin >> destination;
     destination=destination;
-    dijkstra(source);
+    dijkstra(0, destination);
     cout << "¡Excelente! Para llegar a " << carreras[destination] << ", tendrás que pasar por:\n";
     print(destination);
-    cout << carreras[destination] << "\n";
+    cout << "FIN\n"; // indicar el final del camino
     cout << "Y tardarás aproximadamente " << distancia[destination] << " unidades en llegar. Gracias por visitarnos.\n";
 
     return 0;
