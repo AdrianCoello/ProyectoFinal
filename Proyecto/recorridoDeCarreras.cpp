170727d937e2cd50337a6df5fcabe35d29cde625
ponkex9300 <160619372+ponkex9300@users.noreply.github.com>
2024-05-13 10:01:43 -0400

    recorridoDeCarreras.cpp

--- Proyecto/recorridoDeCarreras.cpp
+++ Proyecto/recorridoDeCarreras.cpp
@@ -1,124 +1,94 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define INF 100000010
 using namespace std;
 
-#define INF 30
+vector<pair<int, int>> grafo[18] = {
+    {{9, 1}, {14, 2}, {10, 3}, {15, 4}},
+    {{13, 5}, {11, 6}, {8, 7}, {10, 13}},
+    {{12, 8}, {8, 9}},
+    {},
+    {{15, 10}, {10, 14}, {14, 16}},
+    {},
+    {{8, 11}, {11, 12}},
+    {},
+    {},
+    {{15, 13}},
+    {},
+    {},
+    {},
+    {},
+    {{9, 15}, {11, 16}, {8, 17}},
+    {{10, 5}},
+    {{11, 15}, {14, 17}},
+    {}
+};
 
-vector<pair<int, int>> grafo[18] = {{{1, 1}, {2, 1}, {3, 1}, {4, 1}},
-                                     {{5, 6}, {6, 6}, {7, 6}, {9, 6}, {13, 6}},
-                                     {{8, 8}, {9, 8}},
-                                     {{10, 9}},
-                                     {{6, 8}, {10, 8}, {14, 8}, {16, 8}},
-                                     {{12, 8}, {15, 8}},
-                                     {{11, 8}, {12, 8}},
-                                     {},
-                                     {},
-                                     {{13, 8}},
-                                     {},
-                                     {},
-                                     {},
-                                     {},
-                                     {{15, 8}, {17, 8}},
-                                     {},
-                                     {{15, 11}, {17, 14}},
-                                     {}};
+string carreras[18] = {
+    "UPB", "Administración de Empresas", "Comunicación", "Diseño Gráfico", "Ingeniería Civil", "Analítica Gerencial de Datos",
+    "Ingeniería Comercial", "Psicología Organizacional", "Marketing y Logística", "Derecho", "Arquitectura", "Economía",
+    "Ingeniería Financiera", "Relaciones y Negocios Internacionales", "Ingeniería Electromecánica", "Ingeniería de Sistemas Computacionales",
+    "Ingeniería Industrial y de Sistemas", "Bioingeniería en Recursos Naturales"
+};
 
-string carreras[18] = {"UPB", "Administración de Empresas", "Comunicación", "Diseño Gráfico", "Ingeniería Civil", "Analítica Gerencial de Datos", "Ingeniería Comercial", "Psicología Organizacional", "Marketing y Logística", "Derecho", "Arquitectura", "Economía", "Ingeniería Financiera", "Relaciones y Negocios Internacionales", "Ingeniería Electromecánica", "Ingeniería de Sistemas Computacionales", "Ingeniería Industrial y de Sistemas", "Bioingeniería en Recursos Naturales"};
+void dijkstra(int nodoInicial, vector<int>& distancia, vector<int>& previo) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> colaPrioridad;
+    distancia.assign(18, INF);
+    previo.assign(18, -1);
+    distancia[nodoInicial] = 0;
+    colaPrioridad.push({0, nodoInicial});
 
-int visitados[18]; // marcamos si lo visitamos
-int distancia[18]; // guardamos su distancias para ir comparando
-int previo[18]; 
+    while (!colaPrioridad.empty()) {
+        int nodoActual = colaPrioridad.top().second;
+        int distanciaActual = colaPrioridad.top().first;
+        colaPrioridad.pop();
 
+        for (const auto& vecino : grafo[nodoActual]) {
+            int nodoVecino = vecino.second;
+            int pesoVecino = vecino.first;
 
-void dijkstra(int verticeInicial, int destination)
-{
-    for (int i = 0; i < 18; i++)
-    { // todos empiezan en infinito para comparar
-        distancia[i] = INF;
-        previo[i] = -1;
-    }
-    memset(visitados, 0, sizeof(visitados)); // Todos los nodos como no visitado
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> colaPrioridad;  // ordena por prioridad según las distancias
-    distancia[verticeInicial] = 0;
-               // no recorre nada todavía
-    colaPrioridad.push(make_pair(0, verticeInicial));
-
-    while (!colaPrioridad.empty())
-    {
-        pair<int, int> verticeActual = colaPrioridad.top(); // para dar el valor del primer datp de colaprioridad
-        colaPrioridad.pop();            // borra al que acabamos de recibir su valor
-        int vertice = verticeActual.second;                    // el nodo de donde esta viniendo
-        int peso = verticeActual.first;                        // distancia
-        if (!visitados[vertice])
-        {                              // no pasamos por este antes
-            visitados[vertice] = true; // ya lo visitamos
-            for (int i = 0; i < grafo[vertice].size(); i++)
-            {
-                int verticeVecino = grafo[vertice][i].second;
-                int pesoVecino = grafo[vertice][i].first;
-                if (distancia[verticeVecino] > distancia[vertice] + pesoVecino)
-                { // Relajacion
-                    distancia[verticeVecino] = distancia[vertice] + pesoVecino;
-                    //
-                    previo[verticeVecino]= vertice;
-                    //
-                    colaPrioridad.push(make_pair(distancia[verticeVecino], verticeVecino));
-                }
+            if (distanciaActual + pesoVecino < distancia[nodoVecino]) {
+                distancia[nodoVecino] = distanciaActual + pesoVecino;
+                previo[nodoVecino] = nodoActual;
+                colaPrioridad.push({distancia[nodoVecino], nodoVecino});
             }
         }
     }
 }
 
-void print(int destination) {
-    if (previo[destination] != -1) { // si aun poseo un vertice previo
-        print(previo[destination]);  // recursivamente sigo explorando
+void print(int nodoDestino, const vector<int>& distancia, const vector<int>& previo) {
+    if (distancia[nodoDestino] == INF) {
+        cout << "No hay camino desde " << carreras[0] << " hasta " << carreras[nodoDestino] << endl;
+    } else {
+        vector<int> nodosCamino;
+        for (int i = nodoDestino; i != -1; i = previo[i]) {
+            nodosCamino.push_back(i);
+        }
+
+        cout << "La distancia mínima desde " << carreras[0] << " hasta " << carreras[nodoDestino] << " es: " << distancia[nodoDestino] << endl;
+        cout << "Camino: ";
+        for (int i = nodosCamino.size() - 1; i >= 0; --i) {
+            cout << carreras[nodosCamino[i]];
+            if (i > 0) {
+                cout << " -> ";
+            }
+        }
+        cout << endl;
     }
-    cout << carreras[destination] << " -> "; // mostrar cada paso
 }
 
-int main()
-{
-    cout<<"BIENVENIDO"<< endl;
-    cout<<"Este pequeño programa te ayudara a poder llegar a la carrera que quieras, te dira el camino que necesitas tomar en lacantidad en el menor de tiempo posible, espero te funcione!!\n\n"<<endl;
-    do // <--- Bucle
-    {
-        int option = 0;
-          cout << "Integrantes [1]\n\n" ;
-          cout << "Empezar conociendo las carreras [2]\n\n" ;
-          cout << "Salir [3]\n\n" ;
-        cin >> option;
+int main() {
+    int nodoDestino;
+    cout << "Carreras universitarias:\n";
+    for (int i = 0; i < 18; i++) {
+        cout << i << ": " << carreras[i] << endl;
+    }
+    cout << "Ingrese el número del nodo correspondiente a la carrera a la que desea calcular la distancia mínima desde UPB (carrera inicial): ";
+    cin >> nodoDestino;
 
-        switch(option)
-        {
-        case 1:
-            cout << "Integrantes del Equipo:\n"
-                    "Adrian Coello, Ariana Cordero, Sebastian Nuñez, Valeria Martinez, Diego Orellana\n\n\n";
-            
-              cout << "Despues de conocer a las personas maravillosas que hicieron este programa te invitamos a que veas como funciona:\n";
-             case 2:
-             cout << "Bienvenido a esta guía para conocer las carreras de la UPB\nA continuación podrás observar la lista de carreras disponibles:\n";
-    cout << "0: UPB\n1: Administración de Empresas\n2: Comunicación\n3: Diseño Gráfico\n4: Ingeniería Civil\n5: Analítica Gerencial de Datos\n";
-    cout << "6: Ingeniería Comercial\n7: Psicología Organizacional\n8: Marketing y Logística\n9: Derecho\n10: Arquitectura\n";
-    cout << "11: Economía\n12: Ingeniería Financiera\n13: Relaciones y Negocios Internacionales\n14: Ingeniería Electromecánica\n";
-    cout << "15: Ingeniería de Sistemas Computacionales\n16: Ingeniería Industrial y de Sistemas\n17: Bioingeniería en Recursos Naturales\n";
-    cout << "Ahora añade la carrera final o la carrera que te gustaría conocer: ";
-    int destination;
-    cin >> destination;
-    destination=destination;
-    dijkstra(0, destination);
-    cout << "¡Excelente! Para llegar a " << carreras[destination] << ", tendrás que pasar por:\n\n\n";
-    print(destination);
-    cout << "Destino" << endl;
-    cout << "Y tardarás aproximadamente " << distancia[destination] << " unidades en llegar. Gracias por visitarnos.\n\n\n";
-             break;
-        case 3:
-            cout << "Gracias por usar el programa, esperamos que te haya servido!! :)";
-            return 0; // <--- Rompemos el bucle saliendo
+    vector<int> distancia(18), previo(18);
+    dijkstra(0, distancia, previo); // Nodo inicial es 0 (UPB)
 
-        default:
-            cout << "Opcion no reconocida";
-            break;
-        }
-    } while(true); // El bucle es infinito, sólo sale en el case 3.
+    print(nodoDestino, distancia, previo);
 
     return 0;
 }
